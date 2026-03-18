388a5b2092f77a367b961a53c035e4517ac89604
Ahmed <ahmed17morra@gmail.com>
2026-03-18 14:47:13 +0200

    Fix KafkaLiveTailTest flakiness and re-enable Docker tests
    - Re-enabled Docker-dependent Testcontainers tests (KafkaLiveTailTest and PgEventStoreReaderIntegrationTest) since runner now supports Docker.
    
    - Fixed race condition in KafkaLiveTailTest by replacing thread sleep with continuous event polling.

--- eventlens-pg/src/test/java/io/eventlens/pg/PgEventStoreReaderIntegrationTest.java
+++ eventlens-pg/src/test/java/io/eventlens/pg/PgEventStoreReaderIntegrationTest.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
-@org.junit.jupiter.api.Disabled("Requires Docker")
 @Testcontainers
 class PgEventStoreReaderIntegrationTest {
 
